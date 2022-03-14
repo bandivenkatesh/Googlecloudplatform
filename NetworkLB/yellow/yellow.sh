bb05d9653e696e80f452f5fac93c4ea473d2ab0a
bandivenkatesh <101467293+bandivenkatesh@users.noreply.github.com>
2022-03-14 21:04:47 +0530

    Update yellow.sh

--- NetworkLB/yellow/yellow.sh
+++ NetworkLB/yellow/yellow.sh
@@ -3,5 +3,5 @@ sudo apt-get update
 sudo apt-get install apache2 wget -y
 cd /var/www/html
 rm -rf index.html
-wget <URL>
+wget https://github.com/bandivenkatesh/Googlecloudplatform/blob/main/NetworkLB/yellow/index.html
 sudo service apache2 restart
